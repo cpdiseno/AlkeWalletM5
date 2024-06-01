d38f33ca3dc2b56f0b565a7b8996d6c894b576ca
Camila Peña <camiladiseno@gmail.com>
2024-05-31 22:24:10 -0400

    Se corrige EmptyCase de java a kotlin

--- app/src/main/java/pena/camila/alkewalletm5/view/SendMoney.kt
+++ app/src/main/java/pena/camila/alkewalletm5/view/SendMoney.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import pena.camila.alkewalletm5.R
 import pena.camila.alkewalletm5.databinding.FragmentSendMoneyBinding
 
 class SendMoney : Fragment() {
